6df5f987c8d39da753d3e137966349397a85b035
christopher-besch <christopher.besch@gmx.de>
2021-11-28 11:42:57 +0100

    updated installer

--- install_font.sh
+++ install_font.sh
@@ -4,6 +4,8 @@ IFS=$' \n\t'
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 curl -o font.zip -L https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
+rm -r $HOME/.local/share/fonts
+mkdir $HOME/.local/share/fonts
 mv font.zip $HOME/.local/share/fonts
 cd $HOME/.local/share/fonts
 unzip font.zip
